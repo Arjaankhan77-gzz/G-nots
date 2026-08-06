adf4f88b6bad671a1f859498399356753f627adc
Arjaaan khan <coadwitharjaan728@gmail.com>
2026-08-06 17:52:47 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,8 +3,9 @@
    1. Firebase project: Auth (Email/Password + Google) + Firestore
    2. EmailJS account: used to send the 6-digit signup code by email
    ===================================================================== */
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0T9IRFRDgaM0XLB30IgcYBM3UGxGhnj0",
+  apiKey: "AIzaSyAOT9IRFRDgaMOXlB30IgcYBM3UGxGhnj0",
   authDomain: "g-note-c02d9.firebaseapp.com",
   projectId: "g-note-c02d9",
   storageBucket: "g-note-c02d9.firebasestorage.app",
@@ -12,7 +13,6 @@ const firebaseConfig = {
   appId: "1:514150256929:web:bb0606d31ed17ddafe6e60",
   measurementId: "G-KYPHE8TC6T"
 };
-
 const emailjsConfig = {
   publicKey: "YOUR_EMAILJS_PUBLIC_KEY",
   serviceId: "YOUR_EMAILJS_SERVICE_ID",
